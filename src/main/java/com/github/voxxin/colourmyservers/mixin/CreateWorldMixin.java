cef8e9835195efe050eef1c30185f44d0e23c971
Skylar Redwood <willow.stops.hate@gmail.com>
2023-08-05 18:55:35 +0100

    Massively simplified CreateWorldMixin

--- src/main/java/com/github/voxxin/colourmyservers/mixin/CreateWorldMixin.java
+++ src/main/java/com/github/voxxin/colourmyservers/mixin/CreateWorldMixin.java
@@ -18,28 +18,7 @@ public abstract class CreateWorldMixin {
 
     @Inject(at = @At("HEAD"), method = "Lnet/minecraft/client/gui/screen/world/CreateWorldScreen;createLevelInfo(Z)Lnet/minecraft/world/level/LevelInfo;")
     public void saveNameMixin(boolean debugWorld, CallbackInfoReturnable<LevelInfo> cir) {
-        levelNameField.setText(levelNameField.getText().replace("&0", "§0"));
-        levelNameField.setText(levelNameField.getText().replace("&1", "§1"));
-        levelNameField.setText(levelNameField.getText().replace("&2", "§2"));
-        levelNameField.setText(levelNameField.getText().replace("&3", "§3"));
-        levelNameField.setText(levelNameField.getText().replace("&4", "§4"));
-        levelNameField.setText(levelNameField.getText().replace("&5", "§5"));
-        levelNameField.setText(levelNameField.getText().replace("&6", "§6"));
-        levelNameField.setText(levelNameField.getText().replace("&7", "§7"));
-        levelNameField.setText(levelNameField.getText().replace("&8", "§8"));
-        levelNameField.setText(levelNameField.getText().replace("&9", "§9"));
-        levelNameField.setText(levelNameField.getText().replace("&b", "§a"));
-        levelNameField.setText(levelNameField.getText().replace("&a", "§b"));
-        levelNameField.setText(levelNameField.getText().replace("&c", "§c"));
-        levelNameField.setText(levelNameField.getText().replace("&d", "§d"));
-        levelNameField.setText(levelNameField.getText().replace("&e", "§e"));
-        levelNameField.setText(levelNameField.getText().replace("&f", "§f"));
-
-        levelNameField.setText(levelNameField.getText().replace("&u", "§u"));
-        levelNameField.setText(levelNameField.getText().replace("&l", "§l"));
-        levelNameField.setText(levelNameField.getText().replace("&o", "§o"));
-        levelNameField.setText(levelNameField.getText().replace("&m", "§m"));
-        levelNameField.setText(levelNameField.getText().replace("&k", "§k"));
+        levelNameField.setText(levelNameField.getText().replace("&", "§"));
 
     }
 }
